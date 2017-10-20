378640485c5589461bebc09b7bd8ca5b00120c97
Siguza <siguza@siguza.net>
2017-10-20 23:11:49 +0200

    better root shell

--- src/helper/helper.c
+++ src/helper/helper.c
@@ -1,10 +1,14 @@
-#include <stdio.h>              // perror
+#include <stdio.h>              // perror, asprintf
+#include <stdlib.h>             // getenv
 #include <unistd.h>             // setuid, execve
 
 int main(int argc, char **argv)
 {
     char **exec = argc < 2 ? (char*[]){ "/bin/bash", NULL } : &argv[1];
+    char *path = NULL;
+    asprintf(&path, "PATH=%s", getenv("PATH"));
 #define REQUIRE(expr) do { if(expr) { perror(#expr); return -1; } } while(0)
+    REQUIRE(!path);
     REQUIRE(setuid(0));
-    REQUIRE(execve(*exec, exec, (char*[]){ "EUID=0", "UID=0", "LOGNAME=root", NULL }));
+    REQUIRE(execve(*exec, exec, (char*[]){ "EUID=0", "UID=0", "LOGNAME=root", path, NULL }));
 }
